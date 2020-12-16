6717cabbdd128e235f31522ac69b19f2a3cc1272
LeMagni <72194498+LeMagni@users.noreply.github.com>
2020-12-16 10:03:48 +0100

    Add files via upload

--- Es1.c
+++ Es1.c
@@ -1,83 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <stdbool.h>
-#include <string.h>
-#define LUNG 100
+struct El{
+    int valore;
+    struct El *next;
+};
 
-void insString(char s[]){
-    printf("\nInserire una stringa:\n");
-    fflush(stdin);
-    scanf("%s", s);
-}
+int main(){
+    int n;
+    struct El *lista;
+    struct El *l;
+    lista = NULL; //imposta la lista a null in modo che lista sia vuota
 
-void encoding(char s[]){
-    int k=0, cnt=1;
     do{
-        if(s[k]==s[k+1]){
-            cnt++;
-        }else{
-            if(cnt==1){
-                printf("%c", s[k]);
-            }else{
-                printf("%d", cnt);
-                printf("%c", s[k-1]);
-                cnt=0;
+        printf("Inserisci un naturale o -1 per terminare\n");
+        scanf("%d", &n);
+        if (n >= 0)
+        {
+            if (lista == NULL) //controlla se la lista è vuota
+            {
+                //se lista non punta ancora a una cella viene caricata con l'indirizzo di una cella
+
+                lista = (struct El *)malloc(sizeof(struct El));
+
+                l = lista;
             }
-        }
-        k++;
-    }while(s[k]!='\0');
-    printf("\n");
-}
+            else //se lista punta già a una cella
+            {
+                //assegno a l la cella successiva
 
-void decoding(char s[]){
-    int t=0,k=0;
-    do{
-        t=s[k]-48;
-        printf("%d", t);
-        if(t<=9 && t-48>=0){
-            for(int j=0; j<t; j++){
-                printf("%c", s[k]);
+                l->next = (struct El *)malloc(sizeof(struct El));
+
+                l = l->next;
             }
+            l->valore = n;  //assegno al valore di l n
+            l->next = NULL; // assegno a next di l null 
         }
-        k++;
-    }while(s[k]!='\0');
-    printf("\n");
-}
+    } while (n >= 0);
 
-int menu(int c){
-    char stringa[LUNG];
-    int x;
-
-    printf("DECODING AND ENCODING\n");
-    printf("Menu : \n");
-    printf("1 = Decoding \n");
-    printf("2 = Encoding \n");
-    printf("3 = Esci \n");
-    printf("Inserisci un numero: ");
-    scanf("%d",&x);
-    switch(x){
-        case 1:
-            insString(stringa);
-            decoding(stringa);
-            c=1;
-            break;
-        case 2:
-            insString(stringa);
-            encoding(stringa);
-            c=1; 
-            break;
-        case 3:
-            c=0; 
-            break;
-        }
-    return c;
-}
-
-void main(){
-    int uscita=1;
-    while(uscita==1){
-    uscita=menu(uscita);
+    l = lista; //assegno a l lista per portare l a inzio di lista
+    printf("numeri inseriti: ");
+    while (l != NULL){
+        printf("%d - %p \n", l->valore, l->next);
+        l = l->next; //assegno a next di l l
     }
-    printf("\n>>Premi un tasto per terminare<<\n");
+    printf("\n");
+    return 0;
 }
